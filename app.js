a1b5ae6acbd59c9f61db6f956d2a3af53803dbb3
Taylor Kearns <tkearns@gmail.com>
2016-06-23 11:38:17 -0400

    Deployed app renders index page

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get("/", function(req, res) {
+  res.send("Hello, you must be missing an index.html file!");
 });
 
 app.set("port", (process.env.PORT || 3000));
 
 app.listen(app.get("port"), function() {
-  console.log('Example app listening on port', app.get("port"));
+  console.log("Example app listening on port", app.get("port"));
 });
