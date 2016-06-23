b80ea56ad6ded53d18ee0879ee199113aa903efc
Taylor Kearns <tkearns@gmail.com>
2016-06-23 11:32:13 -0400

    Add browser-bundle.js to deployed code derp

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/', function(req, res) {
 app.set("port", (process.env.PORT || 3000));
 
 app.listen(app.get("port"), function() {
-  console.log('Example app listening on port ', app.get("port"));
+  console.log('Example app listening on port', app.get("port"));
 });
